bdb9678f7f7670772ab71c1471fb86fd7ba71167
MAGANER <maganer.krik@mail.ru>
2023-07-11 22:15:21 +0300

    this class processes state machines

---
+++ GameWindow.cpp
@@ -0,0 +1,54 @@
+#include"GameWindow.h"
+
+
+GameWindow::GameWindow():
+    sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH,WINDOW_HEIGHT),
+                     "Tnake",
+                     sf::Style::Close | sf::Style::Titlebar)
+{
+    setFramerateLimit(10);//otherwise game will work with the light speed
+}
+GameWindow::~GameWindow()
+{
+}
+void GameWindow::run()
+{
+    //start with default state machine
+    auto* current_state_machine = manager.get_current_state_machine();
+    auto event_manager = current_state_machine->get_event_manager();
+
+    while (isOpen())
+    {
+        sf::Event event;
+        while (pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                close();
+        }
+
+        //if it's time to switch to other state machine
+        //then update it and change state machine
+        if (current_state_machine->should_move_to_next())
+        {
+            cout << "move to next!" << endl;
+            manager.update();
+            current_state_machine = manager.get_current_state_machine();
+        }
+
+        //process events, that doesn't require special and additional data
+        if (!event_manager.should_stop())
+            for (auto& e : event_manager.get_independent_events())
+            {
+                if (e->get_c_type() == BaseEvent::ComplexityType::simple)
+                    static_cast<SimpleEvent*>(e)->process();
+            }
+        
+
+        
+        clear();
+        //dependent events are part of game state machine
+        //so they are bound to render
+        current_state_machine->render(*this); 
+        display();
+    }
+}
---
+++ GameWindow.h
@@ -0,0 +1,21 @@
+#ifndef GAME_WINDOW_H
+#define GAME_WINDOW_H
+#include"StateMachine.h"
+
+//use private inheritance, because RenderWindow's methods are used within this class only
+class GameWindow: sf::RenderWindow
+{
+	StateMachineManager manager;
+
+	//use static because it provides ability to make field constexpr
+	static constexpr int WINDOW_WIDTH = (CELL_MAX * CELL_SIZE)+(int)delta*2;
+	static constexpr int WINDOW_HEIGHT = CELL_MAX * CELL_SIZE;
+
+public:
+	GameWindow();
+	~GameWindow();
+	
+	void run();
+};
+
+#endif //GAME_WINDOW_H
