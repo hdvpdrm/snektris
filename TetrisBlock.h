c3063f73e42987b700cd86c375e148f0445fc7e0
MAGANER <maganer.krik@mail.ru>
2023-07-16 17:58:40 +0300

    added ZigZag block

--- TetrisBlock.h
+++ TetrisBlock.h
@@ -153,11 +153,74 @@ public:
 	~VerticalBlock(){}
 
 };
+class ZigZagBlock: public TetrisBLock
+{
+private:
+	bool is_last(const sf::Vector2u& pos1,
+		const sf::Vector2u& pos2)
+	{
+		return pos1.y > pos2.y;
+	}
+	sf::Vector2u find_last()
+	{
+		return  *std::max_element(poses.begin(), poses.end(),
+			[]( sf::Vector2u pos1,
+				sf::Vector2u pos2)
+			{
+				return pos1.y < pos2.y;
+			});
+	}
+	bool can_move(Map* map)
+	{
+		if (being_eaten) return false;
+		if (check_intersects_snake(map))return false;
+
+		auto last = find_last();
+		auto down = sf::Vector2u(last.x, last.y + 1);
+		if (down.y != CELL_MAX - 1)
+		{
+			if (!map->is_empty(down.x, down.y))
+			{
+				return false;
+			}
+			else return true;
+		}
+		else return false;
+	}
+	void generate()
+	{
+		auto x1 = get_random_int(0, CELL_MAX - 4);
+		auto x2 = x1 + 1;
+
+		for (int y = 0; y < 2; y++)
+		{
+			poses.push_back(sf::Vector2u(x1, y));
+		}
+
+		for (int y = 0; y < 2; y++)
+		{
+			poses.push_back(sf::Vector2u(x2, poses[0].y + 1 + y));
+		}
+	}
+public:
+	ZigZagBlock()
+	{
+		generate();
+	}
+	~ZigZagBlock()
+	{
+
+	}
+
+private:
+
+};
 
 static const std::vector<std::function<TetrisBLock* ()>> generators =
 {
 	[]() { return new HorizontalBlock; },
-	[]() { return new VerticalBlock; }
+	[]() { return new VerticalBlock; },
+	[]() { return new ZigZagBlock; }
 };
 static TetrisBLock* generate_tetris_block()
 {
