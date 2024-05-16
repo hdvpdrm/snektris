2377876d208d606c9cb6945a1cdd21243cc3a6e6
hdvpdrm <hardcoderyan@gmail.com>
2024-05-16 15:57:20 +0300

    moved window size constants from GameWindow.h to constants.h

--- GameWindow.h
+++ GameWindow.h
@@ -1,15 +1,15 @@
 #ifndef GAME_WINDOW_H
 #define GAME_WINDOW_H
 #include"StateMachine.h"
+#include"constants.h"
 #include<algorithm>
+
+
 //use private inheritance, because RenderWindow's methods are used within this class only
 class GameWindow: sf::RenderWindow
 {
 	StateMachineManager manager;
 
-	//use static because it provides ability to make field constexpr
-	static constexpr int WINDOW_WIDTH = (CELL_MAX * CELL_SIZE)+(int)delta*2;
-	static constexpr int WINDOW_HEIGHT = (CELL_MAX * CELL_SIZE)-CELL_SIZE;
 
 public:
 	GameWindow();
--- constants.h
+++ constants.h
@@ -12,4 +12,8 @@ static const std::array<sf::Color, 4> colors = {sf::Color::Red,
                                                 sf::Color::Yellow,
                                                 sf::Color::Green,
                                                 sf::Color::Magenta };
+
+static constexpr int WINDOW_WIDTH = (CELL_MAX * CELL_SIZE) + (int)delta * 2;
+static constexpr int WINDOW_HEIGHT = (CELL_MAX * CELL_SIZE) - CELL_SIZE;
+
 #endif
