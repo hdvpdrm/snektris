9339a72e4b8ae800dec0c432bc7fe2cefa300a0d
hdvpdrm <hardcoderyan@gmail.com>
2024-05-16 17:34:10 +0300

    added additional constant representing size of drawable object

--- constants.h
+++ constants.h
@@ -5,15 +5,15 @@
 
 static const int CELL_SIZE = 20;
 static const int CELL_MAX = 30; //max number of cells
-
-static constexpr float delta = 100.0f;
+static const sf::Vector2f DRAWABLE_OBJECT_SIZE = sf::Vector2f(16.0f,16.0f);
+static constexpr float DELTA = 100.0f;
 
 static const std::array<sf::Color, 4> colors = {sf::Color::Red,
                                                 sf::Color::Yellow,
                                                 sf::Color::Green,
                                                 sf::Color::Magenta };
 
-static constexpr int WINDOW_WIDTH = (CELL_MAX * CELL_SIZE) + (int)delta * 2;
+static constexpr int WINDOW_WIDTH = (CELL_MAX * CELL_SIZE) + (int)DELTA * 2;
 static constexpr int WINDOW_HEIGHT = (CELL_MAX * CELL_SIZE) - CELL_SIZE;
 
 #endif
