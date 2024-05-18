618f5cbd629a93df9e5979cd8f52596cdfee1d44
Hdvpdrm <hardcoderyan@gmail.com>
2024-05-18 15:02:10 +0300

    updated build

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-g++ -std=c++20 main.cpp Map.cpp Map.h Matrix.hpp FancyText.hpp FancyText.cpp Event.h constants.h GameWindow.cpp GameWindow.h GetRandomNumber.h MovebaleObject.h Snake.h StateMachine.h TetrisBlock.h -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+g++ -std=c++17 *.cpp *.hpp *.h -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -o snektris
