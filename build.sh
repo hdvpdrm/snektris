d936646ab33d2a3602a5fa1f02f228927d06495a
hdvpdrm <hardcoderyan@gmail.com>
2024-05-22 17:14:29 +0300

    added current dir to include path

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-g++ -std=c++17 *.cpp *.hpp *.h -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -o snektris
+g++ -std=c++17 -I. *.cpp *.hpp *.h -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -o snektris
