9f7d5fe91d9a17dba8c1d857dc1adf2d2f511e17
hdvpdrm <hardcoderyan@gmail.com>
2024-05-26 12:17:05 +0300

    save/load high score data

--- HighScoreManager.cpp
+++ HighScoreManager.cpp
@@ -3,18 +3,91 @@
 
 void HighScoreManager::save()
 {
-
+  std::ofstream ofs(".highscore",std::ios::trunc);
+  ofs<<prepare_data();
+  ofs.close();
 }
 void HighScoreManager::load()
 {
-
+  if(!does_hs_file_exist())
+    {
+      create_hs_file();
+    }
+  else
+    {
+      read_hs_file();
+    }
 }
+bool HighScoreManager::does_hs_file_exist()
+{
+  return fs::exists(fs::path(".highscore"));
+}
+void HighScoreManager::create_hs_file()
+{
+  std::ofstream ofs(".highscore");
+  ofs << ""; 
+  ofs.close();
+}
+void HighScoreManager::read_hs_file()
+{
+  bool ok = true;
+  std::ifstream ifs(".highscore");
+  for(std::string line;getline(ifs,line);)
+    {
+      std::string name, score;
+      ok = split(line,name,score);
+      if(!ok)
+	{
+	  printf("something went wrong while loading .highscore file!\n");
+	  data.clear();
+	  return;
+	}
+      else
+	{
+	  data[name] = atoi(score.c_str());
+	}
+    }
+}
+bool HighScoreManager::split(const std::string& line,std::string& name, std::string& score)
+{
+  int delim_pos = line.find('#');
+  if(delim_pos == std::string::npos) return false;
+  name  = substr(line,0,delim_pos);
+  score = substr(line,delim_pos+1,line.size());
+  if(!is_number(score)) return false;
 
+  return true;
+}
+bool HighScoreManager::is_number(const std::string& str)
+{
+  for(auto& ch:str) if(!isdigit(ch)) return false;
+  return true;
+}
+std::string HighScoreManager::substr(const std::string& str, int a, int b)
+{
+  std::string result;
+  for(int i = a;i<b;++i)
+    {
+      result+=str[i];
+    }
+  return result;
+}
+std::string HighScoreManager::prepare_data()
+{
+  std::string result;
+  for(auto& el: data)
+    {
+      result+=el.first+"#"+std::to_string(el.second)+"\n";
+    }
+  return result;
+}
 HighScoreManager::HighScoreManager()
 {
+  load();
 }
 HighScoreManager::~HighScoreManager()
 {
+  save();
 }
 
 
--- HighScoreManager.h
+++ HighScoreManager.h
@@ -3,19 +3,32 @@
 #include<fstream>
 #include<unordered_map>
 #include<string>
+#include<filesystem>
+#include<stdio.h>
+
+namespace fs=std::filesystem;
 
 class HighScoreManager
 {
 private:
-	std::unordered_map<std::string, int> data;
+  std::unordered_map<std::string, int> data;
 
-	void save();
-	void load();
+  void save();
+  void load();
+
+  inline bool does_hs_file_exist();
+  inline void create_hs_file();
+
+  std::string prepare_data();
+  void read_hs_file();
+  bool split(const std::string& line,std::string& name, std::string& score);
+  std::string substr(const std::string& str, int a, int b);
+  bool is_number(const std::string& str);
 public:
-	HighScoreManager();
-	~HighScoreManager();
+  HighScoreManager();
+  ~HighScoreManager();
 
-	void add(const std::string& name, int score);
+  void add(const std::string& name, int score);
 };
 
 
