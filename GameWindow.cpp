ebe48c8fe8f6281283823212cdd3467c392db71f
hdvpdrm <hardcoderyan@gmail.com>
2024-06-30 13:33:48 +0300

    process pause

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -7,6 +7,7 @@ GameWindow::GameWindow():
                      sf::Style::Close | sf::Style::Titlebar)
 {
     setFramerateLimit(10);//otherwise game will work with the light speed
+    setKeyRepeatEnabled(false);
     
     sf::Image icon;
     icon.loadFromFile("assets/icon.png");
@@ -31,9 +32,17 @@ void GameWindow::run()
         sf::Event event;
         while (pollEvent(event))
         {
+	  
             if (event.type == sf::Event::Closed)
                 close();
-
+	    if(event.type == sf::Event::KeyPressed)
+	      {
+		if(event.key.scancode == sf::Keyboard::Scan::Escape)
+		current_state_machine->set_pause(!current_state_machine->is_paused());
+	      }
+	    if(current_state_machine->is_paused())
+	      continue;
+	    
 	    bool input = manager.is_allowed_to_accumulate_text_input() and
 	      input_counter < USER_NAME_MAX_LEN;
 
@@ -62,9 +71,13 @@ void GameWindow::run()
 	      {
 		if(event.key.scancode == sf::Keyboard::Scan::Backspace)
 		  {
+		    if(static_cast<MainMenu*>(current_state_machine)->get_name_len() == 0)
+		      {}
+		    else{
 		    static_cast<MainMenu*>(current_state_machine)->remove_last_char();
 		    input_counter--;
 		    type_sound.play();
+		    }
 		  }
 	      }
 	    
@@ -77,16 +90,18 @@ void GameWindow::run()
             manager.update();
             current_state_machine = manager.get_current_state_machine();
             event_manager = current_state_machine->get_event_manager();
+	    input_counter = 0;
+	    space_counter = 0;
         }
 
+	
         //process events, that doesn't require special and additional data
-        if (!event_manager.should_stop())
+        if (!event_manager.should_stop() and !current_state_machine->is_paused())
             for (auto& e : event_manager.get_independent_events())
             {
                 if (e->get_c_type() == BaseEvent::ComplexityType::simple)
                     static_cast<SimpleEvent*>(e)->process();
             }
-
         
         clear();
         //dependent events are part of game state machine
