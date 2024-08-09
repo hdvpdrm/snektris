e1ffe0f390f67484d7d3592785830d13c3418706
hdvpdrm <hardcoderyan@gmail.com>
2024-08-09 19:23:18 +0300

    added methods to get width and height

--- Map.h
+++ Map.h
@@ -31,29 +31,33 @@ typedef variant<GameState, void*> cell;
 class Map
 {
 private:
-	Matrix<GameState>* map = nullptr;
+  int width, height;
+  Matrix<GameState>* map = nullptr;
 public:
-	Map(size_t width, size_t height, const sf::Vector2u& snake_head_pos);
-	~Map();
+  Map(size_t width, size_t height, const sf::Vector2u& snake_head_pos);
+  ~Map();
+  
+  //since matrix is incapsulated we provided only special and required methods
+  //to get and set cells of matrix
+  auto get_cell(size_t x, size_t y)
+  {
+    return map->get_elem(x, y);
+  }
+  void set_element(size_t x, size_t y, const GameState& state)
+  {
+    map->set_element(state, x, y);
+  }    
+  
+  stack<sf::Vector2u> get_snake();
+  bool is_snake_at_pos(size_t x, size_t y);
+  bool is_empty(size_t x, size_t y);
+  bool is_apple(size_t x, size_t y);
+  State get_apple_type(size_t x, size_t y);
+  
+  void apply_procedure(const function<void(size_t x, size_t y)>& proc);
 
-	//since matrix is incapsulated we provided only special and required methods
-	//to get and set cells of matrix
-	auto get_cell(size_t x, size_t y)
-	{
-		return map->get_elem(x, y);
-	}
-	void set_element(size_t x, size_t y, const GameState& state)
-	{
-		map->set_element(state, x, y);
-	}    
-        
-	stack<sf::Vector2u> get_snake();
-	bool is_snake_at_pos(size_t x, size_t y);
-	bool is_empty(size_t x, size_t y);
-	bool is_apple(size_t x, size_t y);
-	State get_apple_type(size_t x, size_t y);
-
-	void apply_procedure(const function<void(size_t x, size_t y)>& proc);
+  int get_width(){ return width;}
+  int get_height(){ return height;}
 };
 
 #endif //MAP_H
